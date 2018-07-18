7a44c80388038f22904beb56ebe80d25892b0efc
Keegan Farley <keegomyneego@live.com>
2018-07-18 11:46:30 -0700

    Made simple vue-rx test component - not working as expected

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,42 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1>Hello {{ name }}</h1>
+    <button @click="updateName()">Test out Vue-RX</button>
+    <p>Observable counter: {{ count }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from 'vue'
+import { BehaviorSubject, Observable, timer } from 'rxjs'
+import { take } from 'rxjs/operators'
 
 export default Vue.extend({
   name: 'HelloWorld',
-  props: {
-    msg: String,
+  data () {
+    return {
+      name: 'Vue'
+    }
   },
+  methods: {
+    updateName() {
+      timer(200, 500)
+        .pipe(
+          take(3)
+        )
+        .subscribe(() => {
+          const count$ = < BehaviorSubject < number> > this.$observables.count
+          const oldCount = count$.value
+
+          count$.next(oldCount + 1)
+        })
+    },
+  },
+  subscriptions () {
+    return {
+      count: new BehaviorSubject(0),
+    }
+  }
 });
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import * as VueRx from 'vue-rx'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(VueRx)
 
 new Vue({
   render: h => h(App)
